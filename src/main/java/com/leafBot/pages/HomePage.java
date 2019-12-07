7cec4b929751031b8d3a3922a56cf1763196106c
Baskar <Baskar@DESKTOP-2VSJOD1>
2019-12-07 17:26:53 +0530

    First Push GitHub

--- src/main/java/com/leafBot/pages/HomePage.java
+++ src/main/java/com/leafBot/pages/HomePage.java
@@ -5,7 +5,8 @@ import com.leafBot.testng.api.base.ProjectSpecificMethods;
 public class HomePage extends ProjectSpecificMethods{
 	public HomePage clickLogout() {
 		click(locateElement("class","decorativeSubmit"));
-		return new HomePage();
+			return new HomePage();
+
 	}
 
 }
